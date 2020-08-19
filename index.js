aa4e0b5d350907af93197f5100a0cf26cc6d6477
Ragesh M <mragesh1998@gmail.com>
2020-08-19 16:00:39 +0530

    Add files via upload
    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// Mouse click & Mobile tap
+$(".section").on("click tap", function() {
+	var alphabet = $(this).attr("id");
+	var clickAudio = new Audio("sounds/" + alphabet + ".mp3");
+	playAudio(clickAudio);
+});
+
+// Keyboard Press
+document.addEventListener("keydown", function(e) {
+  var keyPressed = e.key;
+  var keyAudio = new Audio("sounds/" + keyPressed + ".mp3");
+  playAudio(keyAudio);
+  animate(keyPressed);
+});
+
+//Play audio 
+function playAudio(audio) {
+	audio.play();
+}
+
+function animate(button) {
+	$("#" + button).addClass("animate");
+  setTimeout(function() {
+  	$("#" + button).removeClass("animate")
+  }, 200);
+}
+
